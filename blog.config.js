6a702049e061943bcc48b1abc64ee39a17ceee2a
院长 <32212075+yuanter@users.noreply.github.com>
2022-12-05 23:52:24 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: '院长', // 作者
   BIO: '用尽三生烟火，换你一世迷离', // 作者简介
-  LINK: 'https://tangly1024.com', // 网站地址
+  LINK: 'https://vercel.yuanter.top', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
